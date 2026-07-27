81d93d6e63e66f70f157232a2b8ea5d9c7a5d370
T Venkataramana <tvenkataramana805@gmail.com>
2026-07-27 23:17:33 +0530

    Configure Testcontainers for all backend services

--- Order-Service/Order-Service/src/test/java/com/example/Order_Service/OrderServiceApplicationTests.java
+++ Order-Service/Order-Service/src/test/java/com/example/Order_Service/OrderServiceApplicationTests.java
@@ -3,8 +3,27 @@ package com.example.Order_Service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+
 @SpringBootTest
+@Testcontainers
 class OrderServiceApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
 
 	@Test
 	void contextLoads() {
